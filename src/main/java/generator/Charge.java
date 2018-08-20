849606155206af4b4121ca451924f656c7596e70
goulliarts <goulliarts@FR-L2986517.groupinfra.com>
2018-08-20 14:41:44 +0200

    nouvelle proba

--- src/main/java/generator/Charge.java
+++ src/main/java/generator/Charge.java
@@ -1,15 +1,15 @@
 package generator;
 
 public enum Charge {
-	MESSIANISME("de messianisme", 1),
+	MESSIANISME("de messianisme", 5),
 	DEZIANISME("de dezianisMe", 1),
-	JURIANISTE("de jurianiste", 1),
-	MORT("de mort", 1),
-	SORCELLERIE("de sorcellerie", 1),
-	HERESIE("de simple hérésie", 1),
-	ESCLAVAGE("d'esclavagisme illégale", 1),
-	CHARLATANISME("de charlatanisme", 1),
-	COMPLOT("de complot", 1);
+	JURIANISTE("de jurianiste", 5),
+	MORT("de mort", 5),
+	SORCELLERIE("de sorcellerie", 5),
+	HERESIE("de simple hérésie", 5),
+	ESCLAVAGE("d'esclavagisme illégale", 5),
+	CHARLATANISME("de charlatanisme", 5),
+	COMPLOT("de complot", 5);
 
 	private String name;
 	private Integer proba;
